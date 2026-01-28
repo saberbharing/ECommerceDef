a27c2becd565ccb0fcb8da131476d29b8927931e
Saber <saber.bhar.ing@gmail.com>
2026-01-28 11:22:37 +0100

    query

--- src/main/java/gov/tn/defense/repositories/CategoryRepository.java
+++ src/main/java/gov/tn/defense/repositories/CategoryRepository.java
@@ -1,8 +1,11 @@
 package gov.tn.defense.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import gov.tn.defense.entities.Category;
 
@@ -28,5 +31,43 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
 	List<Category> findByLotNumberBetween(int min, int max);
 
 	List<Category> findByNameStartingWithAndLotNumberGreaterThan (String name, int number);
+	
+	List<Category> findByEditionBefore(Date avant);
+	
+	List<Category> findByEditionAfter(Date apres);	
+
+	List<Category> findByEditionBetween(Date avant, Date apres);
+	
+	List<Category> findByNameStartingWithIgnoreCaseAndLotNumberBetweenAndEditionBefore
+		(String name, int min, int max, Date edition);
+	
+	
+	//JPQL
+	//SQL
+	
+	@Query("select c from Category c "
+			+ " where c.name = :name "
+			+ " and c.lotNumber> :lotNumber "
+			+ " and c.edition< :edition")
+	List<Category> findByMultipleCriteriaJPQL(@Param("name") String name,
+			@Param("edition") Date edition, 
+			@Param("lotNumber") int min);	
+	
+
+	@Query(nativeQuery = true, value = "select * from categories c "
+			+ " where c.name Like :name% "
+			+ " and c.lot_number> :lotNumber "
+			+ " and c.edition< :edition")
+	List<Category> findByMultipleCriteriaSQL(@Param("name") String name,
+			@Param("edition") Date edition, 
+			@Param("lotNumber") int min);
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
